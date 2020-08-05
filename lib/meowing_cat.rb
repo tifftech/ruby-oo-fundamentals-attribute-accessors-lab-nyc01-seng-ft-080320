af2cfb33229e343da706a88253aa73d9264a215c
Tiffani Best <tiffanimelissabest@gmail.com>
2020-08-05 02:25:45 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  
-  
+  def initialize(name, age, location)
+    @name = name
+    @age = age
+    @location = location
+  end
+  def name=(name)
+    @name = name
+  end
+  def name
+    @name
+  end
+  def speak
+    puts "Meow!"
+  end
+
+end
